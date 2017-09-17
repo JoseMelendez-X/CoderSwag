0a61d8f3c3a9e2554306af234a3002b15ecfff7a
Jose Melendez <JoseM_Melendez@icloud.com>
2017-09-17 12:45:32 -0400

    Added functions and data

--- CoderSwag/CoderSwag/Services/DataService.swift
+++ CoderSwag/CoderSwag/Services/DataService.swift
@@ -26,8 +26,98 @@ class DataService {
         
     ]
     
+    //Products of hats
+    private let hats = [
+        
+        Product(title: "Devslopes logo graphic beani", price: "18.0", imageName: "hat01.png"),
+        
+        Product(title: "Devslopes logo hat black", price: "$22.00", imageName: "hat02.png"),
+        
+        Product(title: "Devslopes log0 hat white", price: "$22.00", imageName: "hat03.png"),
+        
+        Product(title: "Devslopes logo SnapBack", price: "$20.00`", imageName: "hat04.png")
+    ]
+    
+    //Product of hoodies
+    private let Hoodies = [
+        
+        Product(title: "Devslopes logo hoodie grey", price: "32", imageName: "hoodie01.png"),
+        
+        Product(title: "Devslopes logo hoodie red", price: "32", imageName: "hoodie02.png"),
+        
+        Product(title: "Devslopes hoodie grey", price: "32", imageName: "hoodie03.png"),
+        
+        Product(title: "Devslopes hoodie black", price: "32", imageName: "hoodie04.png"),
+    ]
+    
+    //Product of shirts
+    private let shirts = [
+        
+         Product(title: "Devslopes logo shirt black", price: "$18", imageName: "shirt01.png"),
+        
+         Product(title: "Devslopes Badge shirt light grey", price: "$19", imageName: "shirt02.png"),
+        
+         Product(title: "Devslopes logo shirt red", price: "$18", imageName: "shirt03.png"),
+        
+         Product(title: "Hustle Delegate Grey", price: "$18", imageName: "shirt04.png"),
+        
+         Product(title: "Kickflip studios black", price: "$18", imageName: "shirt05.png")
+    
+    ]
+    
+    private let digitalGoods = [Product]()
+    
     func getCategories() -> [Category] {
         
         return categories
+    }
+    
+    func getProducts(forCategoryTitle title: String ) -> [Product] {
+        
+        switch title {
+        case "SHIRTS":
+        return getShirts()
+            
+       
+        case "HATS":
+        return getHats()
+                
+        
+        case "HOODIES":
+        return getHoodies()
+                    
+        
+        case "DIGITAL":
+        return getDigital()
+                        
+            
+        default:
+        return getShirts()
+            
+        }
+    }
+
+    func getHats() -> [Product]{
+        
+        return hats
+        
+    }
+    
+    func getHoodies() -> [Product]{
+        
+        return Hoodies
+        
+    }
+    
+    func getShirts() -> [Product]{
+        
+        return shirts
+        
+    }
+    
+    func getDigital() -> [Product] {
+        
+        return digitalGoods
+        
     }
 }
---
+++ CoderSwag/CoderSwag/View/ProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Jose Melendez on 9/16/17.
+//  Copyright © 2017 JoseMelendez. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var productImage: UIImageView!
+    
+    @IBOutlet weak var productPrice: UILabel!
+    
+    @IBOutlet weak var productTitle: UILabel!
+}
